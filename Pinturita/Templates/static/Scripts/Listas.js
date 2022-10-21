428791cc45953deed893435815f097de58940a89
Benjabriel <gbvaldez81@gmail.com>
2022-10-20 20:23:26 -0500

    Cambie el script de javascript

--- Pinturita/Templates/static/Scripts/Listas.js
+++ Pinturita/Templates/static/Scripts/Listas.js
@@ -1,5 +1,84 @@
 ( function (){
+var Emergente=document.getElementById("Emergente");
+var Emergcopia= Emergente.cloneNode(true);
+Emergente.remove()
+    
 var Formulario= document.getElementsByClassName("Formulario")[0];
+function Desenfocar(){
+    document.activeElement.blur();
+}
+
+Formulario.addEventListener("focusout",(event)=>{
+    if (event.target.classList.contains("Listas")){
+        let IDBUSCAR=event.target.getAttribute("list");
+        let DATOS= document.getElementById(IDBUSCAR);
+        var Tama_List=DATOS.children.length;
+        var Paro=true;
+        for(j=0;j<Tama_List;j++){
+            let InputEntrante=DATOS.children[j].getAttribute("value")
+            if(InputEntrante.length==1){
+                Paro=false; 
+                break;
+            }
+            
+            if(InputEntrante == event.target.value){
+                Paro=false; 
+                break;
+            }
+
+
+        }
+        if (Paro){
+            document.body.appendChild(Emergcopia);
+            Emergente=document.getElementById("Emergente");
+            document.getElementById("Emergente_Elemento").innerText=event.target.value;
+            let Boton = document.getElementById("Emergente_exit")
+            document.getElementById("Emergente_enlace").setAttribute("href","/Edicion/Ingresar_unico/"+ event.target.dataset.archivo+"/"+event.target.value +"/" + event.target.dataset.direccion)
+            //document.getElementById("Emergente_enlace").setAttribute("href",URLactual)
+            Boton.addEventListener("click",()=>{
+                Emergente.remove();
+                event.target.value="";
+            })
+            setTimeout(Desenfocar,2);
+            
+        }
+    }
+})
+
+Formulario.addEventListener("submit",(e)=>{
+    var Listas= document.getElementsByClassName("Listas");
+    for (var i=0 ; i< Listas.length;i++){
+        var Parar=true
+        let IDBUSCAR=Listas[i].getAttribute("list");
+        var Listdata= document.getElementById(IDBUSCAR);
+        var Tama_List=Listdata.children.length;
+        for(j=0;j<Tama_List;j++){
+            if(Listdata.children[j].getAttribute("value") == Listas[i].value){
+                Parar=false;
+                break;
+            }
+        }
+        if (Parar){
+            var Etiqueta= Listas[i].previousElementSibling.previousElementSibling      
+            break;
+        } 
+        
+    }
+    if (Parar){
+        //alert("Error: Valor invalido\nEn " + Etiqueta.textContent + " elije un valor de la lista")
+        e.preventDefault();
+    }
+    else{
+        for (var i=0;i< Listas.length;i++ ){
+            Listas[i].value=Listas[i].value.split(",")[1]
+        }
+    }
+    
+    
+
+})
+
+/*
 var Listas= document.getElementsByClassName("Listas");
 var Emergente=document.getElementById("Emergente");
 var Emergcopia= Emergente.cloneNode(true);
@@ -78,6 +157,6 @@ Formulario.addEventListener("submit",(e)=>{
     
 
 })
-
+*/
 
 })()
